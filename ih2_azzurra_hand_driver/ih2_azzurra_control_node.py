3a1911c39648b787453b5d2fed6a402f39567f9c
Ahmed Abdelrahman <37597383+af-a@users.noreply.github.com>
2026-02-24 13:24:10 +0100

    [ih2_node] Remove extra param

--- ih2_azzurra_hand_driver/ih2_azzurra_control_node.py
+++ ih2_azzurra_hand_driver/ih2_azzurra_control_node.py
@@ -34,13 +34,11 @@ class Ih2AzzurraControlNode(Node):
         self.declare_parameter('action_command_topic', '~/action_command')
         self.declare_parameter('joint_states_topic', '~/joint_states')
         self.declare_parameter('pose_config_file_path', '/home/ahmed/workspace/ros2_ws/src/ih2_azzurra_hand_driver/config/default_hand_poses.yaml')
-        self.declare_parameter('debug', False)
 
         self.serial_port = self.get_parameter('serial_port').value
         self.action_command_topic = self.get_parameter('action_command_topic').value
         self.joint_states_topic = self.get_parameter('joint_states_topic').value
         self.pose_config_file_path = self.get_parameter('pose_config_file_path').value
-        self.debug = self.get_parameter('debug').value
 
         # Initialize subscribers:
         self.action_command_subscription = self.create_subscription(String,
