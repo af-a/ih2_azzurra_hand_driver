3f6ddf0b0b3aaf38e21309e6f26fa4bfae7536fa
Ahmed Abdelrahman <37597383+af-a@users.noreply.github.com>
2026-02-27 09:40:06 +0100

    Add: shutdown all nodes in launch file if main node dies

--- launch/ih2_driver.launch.py
+++ launch/ih2_driver.launch.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument 
+from launch.actions import DeclareLaunchArgument, Shutdown
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
@@ -32,13 +32,14 @@ def generate_launch_description():
             {'serial_port': LaunchConfiguration('serial_port')},
             {'action_command_topic': LaunchConfiguration('action_command_topic')},
         ],
+        on_exit=Shutdown(),
     )
 
     rqt_reconfigure_node = Node(
         package='rqt_reconfigure',
         executable='rqt_reconfigure',
         name='ih2_rqt_reconfigure',
-        arguments=[f'/{prensilia_control_node_name}']
+        arguments=[f'/{prensilia_control_node_name}'],
     )
 
     return LaunchDescription([
