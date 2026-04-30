66bc39a94786aa19879e6033f3477665b10a680d
Ahmed Abdelrahman <37597383+af-a@users.noreply.github.com>
2026-04-30 15:48:09 +0200

    [ih2_node] Add motor currents to hand state msg

--- ih2_azzurra_hand_driver/ih2_azzurra_control_node.py
+++ ih2_azzurra_hand_driver/ih2_azzurra_control_node.py
@@ -114,10 +114,13 @@ class Ih2AzzurraControlNode(Node):
     def hand_state_timer_callback(self):
         self.hand_state_msg.header.stamp = self.get_clock().now().to_msg()
         self.hand_state_msg.motor_position = [value for value in self.hand_controller.get_pose()]
-        self.hand_state_publisher.publish(self.hand_state_msg)
 
         finger_status = self.hand_controller.get_finger_status()
-        self.hand_state_msg.moving = [bool(int(status_bits[-1])) for status_bits in finger_status]
+        self.hand_state_msg.motor_moving = [bool(int(status_bits[-1])) for status_bits in finger_status]
+
+        self.hand_state_msg.motor_current = self.hand_controller.get_motor_currents()
+
+        self.hand_state_publisher.publish(self.hand_state_msg)
 
     def action_command_callback(self, msg):
         self.get_logger().info(f'Received action command message: {msg.data}')
